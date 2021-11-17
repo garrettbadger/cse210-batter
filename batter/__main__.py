8d991cdfb88f636d6c3047d7d852adc88a49ed62
garrettbadger <garrett.badger.id@gmail.com>
2021-11-17 15:36:14 -0700

    got bricks to show up

--- batter/__main__.py
+++ batter/__main__.py
@@ -1,4 +1,6 @@
 import os
+
+# from batter.game.constants import BRICK_HEIGHT, BRICK_WIDTH
 os.environ['RAYLIB_BIN_PATH'] = r'C:\Users\flipp\cse-210-solo-checkpoints\cse210-student-solo-checkpoints\07-snake\raylib-2.0.0-Win64-mingw\raylib-2.0.0-Win64-mingw\lib'
 import random
 from game import constants
@@ -27,21 +29,51 @@ def main():
     # create the cast {key: tag, value: list}
     cast = {}
 
-    cast["bricks"] = []
+    
     # TODO: Create bricks here and add them to the list
-    brick1 = Brick()
-    brick1.set_position(Point(10,10))
-    cast["bricks"].append(brick1)
-    brick2 = Brick()
-    brick2.set_position(Point(100,10))
-    cast["bricks"].append(brick2)
-    brick3 = Brick()
-    brick3.set_position(Point(200,10))
-    cast["bricks"].append(brick3)
+    
+    bricks = []
+    for x in range(20, 750, constants.BRICK_WIDTH):
+        for y in range(0, 200, constants.BRICK_HEIGHT):
+            brick = Brick()
+            position = Point(x, y)
+            brick.set_position(position)
+            brick.set_image(constants.IMAGE_BRICK)
+            bricks.append(brick)
 
-    for brick in cast["bricks"]:
-        print(brick.get_position()._x)
-        print(brick.get_position()._y)
+            
+    
+    cast['bricks'] = bricks
+
+        
+        
+
+
+
+
+    # brick1 = Brick()
+    # brick1.set_position(Point(10,10))
+    # brick1.set_image(constants.IMAGE_BRICK)
+    # brick1.set_height(constants.BRICK_HEIGHT)
+    # brick1.set_width(constants.BRICK_WIDTH)
+    # cast["bricks"].append(brick1)
+    
+    # brick2 = Brick()
+    # brick2.set_position(Point(100,10))
+    # brick2.set_image(constants.IMAGE_BRICK)
+    # brick2.set_height(constants.BRICK_HEIGHT)
+    # brick2.set_width(constants.BRICK_WIDTH)
+    # cast["bricks"].append(brick2)
+    # brick3 = Brick()
+    # brick3.set_position(Point(200,10))
+    # brick3.set_image(constants.IMAGE_BRICK)
+    # brick3.set_height(constants.BRICK_HEIGHT)
+    # brick3.set_width(constants.BRICK_WIDTH)
+    # cast["bricks"].append(brick3)
+
+    # for brick in cast["bricks"]:
+    #     print(brick.get_position()._x)
+    #     print(brick.get_position()._y)
 
 
     cast["balls"] = []
--- batter/game/brick.py
+++ batter/game/brick.py
@@ -1,9 +1,13 @@
+from game import constants
 from game.actor import Actor
 from game.point import Point
 
 class Brick(Actor):
     def __init__(self):
         super().__init__()
-    pass
+        self._brick_image = constants.IMAGE_BRICK
+        self._brick_height = constants.BRICK_HEIGHT
+        self._brick_width = constants.BRICK_WIDTH
+
         
     
